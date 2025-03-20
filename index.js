5dfee914594919663ea870231efb2194a0231802
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-03-20 13:21:21 +0200

    graphql

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express')
-const expressGraphQL = require("express-graphql")
+const expressGraphQL = require('express-graphql')
 const app = express();
+const {GraphQLSchema} = require("graphql")
+
+
 const PORT = 5000
+
+app.use('/graphql', expressGraphQL({
+    graphql:true
+}))
+
 
 app.listen(PORT,()=>{
     console.log(`Server running in port ${PORT}`)
