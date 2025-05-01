ed7112e4c5af70ad73156ac2cbdd051137b6ae52
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-05-02 01:27:29 +0200

    complete server setup

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log({ starting: true })
+console.log({ starting: true });
+
+const express = require('express');
+const app = express();
+
+app.use('/graphql', (req, res) => {
+    res.send({ data: true })
+});
+
+app.listen(5000, () => {
+    console.log(`Server running on port 5000`)
+});
