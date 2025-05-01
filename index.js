aa986deab83c5849663d2605390b04f69c4a3015
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-05-01 22:55:59 +0200

    commit hello field

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
-const app = express();
 
 //needed to create a query
 const { GraphQLSchema,
@@ -8,25 +7,41 @@ const { GraphQLSchema,
     GraphQLString
 } = require("graphql")
 
+const app = express();
+
 const PORT = 5000
 
-const schema = new GraphQLSchema({
-    query: new GraphQLObjectType({
-        name: 'Hello World',
-        fields: () => ({
-            message: {
-                type: GraphQLString,
-                resolve: () => `Hello World`
+
+const query = new GraphQLObjectType({
+    name: 'HelloWorld',
+    description: 'The root query',
+    fields: {
+        /*viewer: {
+            type: GraphQLString,
+            resolve() {
+                return 'viewer';
             }
-        })
-    })
-})
+        },*/
+        hello:{
+            type:GraphQLString,
+            resolve(){
+                return 'Hello World!'
+            }
+        }
+    }
+});
+
+const schema = new GraphQLSchema({
+    query: query
+});
+
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
-    graphql: true
+    graphiql:true //enables the GraphiQL UI for testing
 }))
 
+app.use(express.json());
 
 app.listen(PORT, () => {
     console.log(`Server running in port ${PORT}`)
