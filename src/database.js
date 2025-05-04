879d27089f2c51a9c768e68e463a3445f47e5e56
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-05-04 21:49:01 +0200

    copy data from data.json to database

--- src/database.js
+++ src/database.js
@@ -1,2 +1,18 @@
 const sqlite = require('sqlite');
 const tables = require('./tables');
+
+export const db = new sqlite.Database('./db.sqlite');
+
+export const getSql = (query) => {
+    return new Promise((resolve, reject) => {
+        console.log(query.text);
+        console.log(query.values);
+        db.all(query.text, query.values, (err, rows) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(rows);
+            }
+        });
+    });
+}
