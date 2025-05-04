1b2617b8e5007aa7a8c5b25764a951139c83da48
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-05-05 00:27:48 +0200

    commit create database function

--- src/seedData.js
+++ src/seedData.js
@@ -1,3 +1,19 @@
 const data = require('./data');
 const tables = require('./tables');
 const database = require('./database');
+
+const sequencePromises = function (promises) {
+    return promises.reduce((promise, promiseFunction) => {
+        return promise.then(() => {
+            return promiseFunction();
+        });
+    }, Promise.resolve());
+}
+
+const createDatabase = () => {
+    let promises = [tables.users, tables.usersFriends, tables.posts].map((table) => {
+        return () => database.getSql(table.create().toQuery());
+    });
+
+    return sequencePromises(promises);
+}
